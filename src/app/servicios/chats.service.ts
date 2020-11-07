cf5ded57a2f92f5e6ceee2ff0a6c20b6d130cde4
Rober Martinez Bejarano <rober322@hotmail.es>
2020-11-07 08:18:34 -0500

    firabase library import solution

--- src/app/servicios/chats.service.ts
+++ src/app/servicios/chats.service.ts
@@ -1,45 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
+import * as firebase from "firebase";
 import { map } from "rxjs/operators";
-import { message } from '../models/message';
-import { firestore } from 'firebase';
+import { message } from "../models/message";
 
-
-
-export interface chat{
-  description: string
-  name: string
-  id: string
-  img: string
+export interface chat {
+  description: string;
+  name: string;
+  id: string;
+  img: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChatsService {
+  constructor(private db: AngularFirestore) {}
 
-  constructor(private db : AngularFirestore) { }
-
-  getChatRooms(){
-    return this.db.collection('chatRooms').snapshotChanges().pipe(map(rooms => {
-      return rooms.map(a =>{
-        const data = a.payload.doc.data() as chat;
-        data.id = a.payload.doc.id;
-        return data;
-      })
-    }))
+  getChatRooms() {
+    return this.db
+      .collection("chatRooms")
+      .snapshotChanges()
+      .pipe(
+        map((rooms) => {
+          return rooms.map((a) => {
+            const data = a.payload.doc.data() as chat;
+            data.id = a.payload.doc.id;
+            return data;
+          });
+        })
+      );
   }
 
-  getChatRoom(chat_id: string){
-    return this.db.collection('chatRooms').doc(chat_id).valueChanges()
+  getChatRoom(chat_id: string) {
+    return this.db.collection("chatRooms").doc(chat_id).valueChanges();
   }
 
-  sendMsgToFirebase(message: message, chat_id: string){
-    this.db.collection('chatRooms').doc(chat_id).update({
-      messages : firestore.FieldValue.arrayUnion(message),
-    })
-    
+  sendMsgToFirebase(message: message, chat_id: string) {
+    this.db
+      .collection("chatRooms")
+      .doc(chat_id)
+      .update({
+        messages: firebase.default.firestore.FieldValue.arrayUnion(message),
+      });
   }
-
-
 }
